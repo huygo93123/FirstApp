a0785bc3c983ab86e3c561547fdd6a24d16759dd
huygo93123 <huydk1@gmail.com>
2017-12-20 16:09:41 +0700

    update

--- app/src/main/java/huydk/firstapp/MainActivity.java
+++ app/src/main/java/huydk/firstapp/MainActivity.java
@@ -2,13 +2,42 @@ package huydk.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lvThu;
+    String []arrThu;
+    ArrayAdapter<String>adapterThu;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        lvThu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,"Ban chon array thu " + arrThu[i] + " : ",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void addControls() {
+        arrThu = getResources().getStringArray(R.array.arrThu);
+        adapterThu = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_expandable_list_item_1,
+                arrThu);
+        lvThu = findViewById(R.id.lvThu);
+        lvThu.setAdapter(adapterThu);
     }
 
 }
