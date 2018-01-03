251b1c940c962b821de6a341de90260bc403ef61
huygo93123 <huydk1@gmail.com>
2018-01-03 21:44:13 +0700

    first commit

--- app/src/main/java/huydk/firstapp/MainActivity.java
+++ app/src/main/java/huydk/firstapp/MainActivity.java
@@ -1,14 +1,90 @@
 package huydk.firstapp;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtDate,txtTime;
+    ImageButton btnDate,btnTime;
+
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xuLyHienThiDatePicker();
+            }
+        });
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xyLyHienThiTimePicker();
+            }
+        });
+    }
+
+    private void xyLyHienThiTimePicker() {
+        TimePickerDialog.OnTimeSetListener listener= new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                calendar.set(Calendar.HOUR,i);
+                calendar.set(Calendar.MINUTE,i1);
+                txtTime.setText(sdf2.format(calendar.getTime()));
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this,listener,calendar.get(Calendar.MINUTE),calendar.get(Calendar.HOUR),true);
+        timePickerDialog.show();
+    }
+
+    private void xuLyHienThiDatePicker() {
+        DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR,i);
+                calendar.set(Calendar.MONTH,i1);
+                calendar.set(Calendar.DATE,i2);
+                txtDate.setText(sdf1.format(calendar.getTime()));
+            }
+        };
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,listener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DATE));
+
+        datePickerDialog.show();
+    }
+
+    private void addControls() {
+        txtDate = findViewById(R.id.txtDate);
+        txtTime = findViewById(R.id.txtTime);
+
+        btnDate = findViewById(R.id.btnDate);
+        btnTime = findViewById(R.id.btnTime);
+
+        calendar = Calendar.getInstance();
+        txtDate.setText(sdf1.format(calendar.getTime()));
+        txtTime.setText(sdf2.format(calendar.getTime()));
     }
 
 }
